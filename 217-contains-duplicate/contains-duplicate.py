f425e06450282141f8afddd16136973d6bbdf3d8
Varad1310 <101795643+Varad1310@users.noreply.github.com>
2024-07-22 19:25:46 +0530

    Time: 466 ms (6.89%) | Memory: 28.2 MB (95.40%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,11 +1,8 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
         nums.sort()
-        for i in range(1,len(nums)):
-            if (nums[i]==nums[i-1]):
+        for i in range(0,len(nums)-1):
+            if nums[i]^nums[i+1]==0:
                 return True
         return False
+        
