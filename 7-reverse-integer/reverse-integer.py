6fbdba097c66bc622b78aebf3646cf543d92a268
Varad1310 <101795643+Varad1310@users.noreply.github.com>
2024-08-17 20:01:18 +0530

    Time: 16 ms (56.76%) | Memory: 11.7 MB (6.76%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.py
@@ -0,0 +1,20 @@
+from copy import deepcopy
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        num=abs(x)
+        reverse=0
+        
+        while(num>0):
+            digit=num%10
+            reverse=reverse*10+digit
+            num=num/10
+        if reverse>=2**31-1 or reverse<-2**31:
+            return 0
+        if x<0:
+            return reverse*-1
+        else:
+            return reverse
