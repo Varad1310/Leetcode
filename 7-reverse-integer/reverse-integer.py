384f3edbd0a32b42c3d0dbf42e61f28c15ae1bd1
Varad1310 <101795643+Varad1310@users.noreply.github.com>
2024-08-17 20:03:49 +0530

    Time: 17 ms (48.34%) | Memory: 11.6 MB (67.25%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -7,12 +7,11 @@ class Solution(object):
         """
         num=abs(x)
         reverse=0
-        
         while(num>0):
             digit=num%10
             reverse=reverse*10+digit
             num=num/10
-        if reverse>=2**31-1 or reverse<-2**31:
+        if reverse>2**31-1 or reverse<-2**31:
             return 0
         if x<0:
             return reverse*-1
